9e42fc694048a19490084c3ba48b5abda901aa50
manh dino <manhnguyen1238@gmail.com>
2023-01-04 13:00:40 +0700

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L02_Variables // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
